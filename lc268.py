d64d611b5208543e46247c64f1b7b1bcb19a6ece
Dai-MacOS <1097249217@qq.com>
2022-07-05 18:27:33 +0900

    异或运算：异或0为自身，异或自身为0

---
+++ lc268.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res=0
+        for i in range(len(nums)):
+            res ^= i^nums[i]
+        return res^len(nums)
+        
